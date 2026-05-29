4f35cd1c7b86d329eef1a9ad165cb0f9b0b6a2b5
ThirietIzya <izya.thiriet@edu.univ-fcomte.fr>
2026-05-29 21:57:30 +0200

    izya+accueil+icons+image+autres

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        orange: '#FF6200',
+        orange: {
+          50: '#fff7ed',
+          100: '#ffedd5',
+          200: '#fed7aa',
+          300: '#fdba74',
+          400: '#fb923c',
+          500: '#FF6200',
+          600: '#ea580c',
+          700: '#c2410c',
+          800: '#9a360d',
+          900: '#7c2d12',
+        }
       },
       boxShadow: {
         orange: '0 30px 90px rgba(255,98,0,0.16)',
